59c26ab9a9f4a6444a46922de3af766ed96a3698
Hendrik Jennen <jennen.hendrik@gmail.com>
2020-11-28 11:06:42 +0100

    Initial commit

---
+++ src/Lens.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Control.Lens
+{
+    public class Lens<O, P>
+    {
+        public Func<O, P> Get { get; }
+
+        public Func<P, Func<O, O>> Set { get; }
+
+        public Lens(Func<O, P> get, Func<P, Func<O, O>> set)
+        {
+            Get = get;
+            Set = set;
+        }
+    }
+
+    public static class Create
+    {
+        public static Lens<O, Q> Compose<O, P, Q>(
+            this Lens<O, P> op,
+            Lens<P, Q> pq)
+            => new Lens<O, Q>(
+                o => pq.Get(op.Get(o)),
+                q => o => op.Set(pq.Set(q)(op.Get(o)))(o));
+
+        public static Lens<IEnumerable<O>, O> Pos<O>(int i)
+            => new Lens<IEnumerable<O>, O>(
+                l => l.ElementAt(i),
+                o => l => l.Take(i).Append(o).Concat(l.Skip(i + 1)));
+    }
+}
---
+++ test/LensTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Immutable;
+using Control.Lens;
+using Xunit;
+
+namespace ControlTest
+{
+    public class Person : IEquatable<Person>
+    {
+        public string Name { get; }
+        public int Age { get; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override bool Equals(object obj)
+            => Equals(obj as Person);
+
+        public bool Equals(Person p)
+            => p != null
+                && Name == p.Name
+                && Age == p.Age;
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Age);
+        }
+
+        public Person With(string name = null, int? age = null)
+            => new Person(
+                name ?? Name,
+                age == null ? Age : age.Value);
+
+        public override string ToString()
+            => $"Name: {Name}, Age: {Age}";
+    }
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var persons = ImmutableArray.Create(
+                new Person("John", 33),
+                new Person("Ada", 25));
+
+            var name = new Lens<Person, string>(
+                p => p.Name,
+                n => p => p.With(name: n));
+            var pos = Create.Pos<Person>(0); 
+
+            var sut = pos.Compose(name).Set("Oliver");
+            var actual = sut(persons);
+
+            var expected = ImmutableArray.Create(
+                new Person("Oliver", 33),
+                new Person("Ada", 25));
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
